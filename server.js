f4adc9d801cdaa2366766ad27c45d4324514e81b
emily <eap6787@gmail.com>
2022-04-05 10:39:22 -0600

    add database connection

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Atlas-2021',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (not found)
 app.use((req, res) => {
